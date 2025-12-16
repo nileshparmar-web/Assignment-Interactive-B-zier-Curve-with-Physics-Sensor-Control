62de823b59fe7721d08a779a0be16f201bb7a23c
Nilesh Parmar <pn6154880@gmail.com>
2025-12-16 14:40:44 +0530

    Add files via upload
    Adjust canvas size on window resize

---
+++ script.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
