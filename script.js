bdac8d397458032f635c5aff631d9c0c3ca251f3
Nilesh Parmar <pn6154880@gmail.com>
2025-12-16 14:41:58 +0530

    Add control points for canvas resizing
    Added control points for canvas manipulation.

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ function resizeCanvas() {
   canvas.height = window.innerHeight;
 }
 window.addEventListener('resize', resizeCanvas);
+
 resizeCanvas();
+
+let P0 = { x: 80, y: canvas.height / 2 };
+let P3 = { x: canvas.width - 80, y: canvas.height / 2 };
+
+let P1 = {
+  x: 250,
+  y: canvas.height / 2 - 120,
+  vx: 0,
+  vy: 0,
+  target: { x: 250, y: canvas.height / 2 - 120 }
+};
+
+let P2 = {
+  x: canvas.width - 250,
+  y: canvas.height / 2 - 120,
+  vx: 0,
+  vy: 0,
+  target: { x: canvas.width - 250, y: canvas.height / 2 - 120 }
+};
+
+let dragPoint = null;
